060e208e441609be3cf3878bd6479e06dc988572
FeelsGo-od <andrew.sokolowsky@gmail.com>
2024-05-25 21:44:56 +0200

    Fixing issues with popup auth

--- src/index.js
+++ src/index.js
@@ -12,18 +12,6 @@ if ('serviceWorker' in navigator) {
     });
 }
 
-document.querySelectorAll('.progress-bar').forEach(bar => {
-    bar.addEventListener('input', function() {
-        const percentage = document.getElementById(`percentage-${this.id.split('-')[2]}`);
-        if (percentage) {
-            percentage.textContent = `${this.value}%`;
-            percentage.style.width = `${this.value}%`;
-        } else {
-            console.error('Element not found:', `percentage-${this.id.split('-')[2]}`)
-        }
-    });
-});
-
 async function getConfig() {
     const response = await fetch('/.netlify/functions/config');
     if (!response.ok) {
@@ -33,7 +21,7 @@ async function getConfig() {
     return config.firebaseConfig;
 }
 
-let currentUser = null;
+// let currentUser = null;
 
 getConfig().then(firebaseConfig => {
     // Initialize Firebase with the fetched config
@@ -42,6 +30,45 @@ getConfig().then(firebaseConfig => {
         const auth = getAuth(app);
         const provider = new GoogleAuthProvider();
         const db = getFirestore(app);
+
+        document.getElementById('login-btn').addEventListener('click', () => {
+            signInWithPopup(auth, provider).then(result => {
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                const user = result.user;
+                console.log('User signed in: ', user);
+            }).catch(error => {
+                console.error('Error during sign in:', error);
+                if (error.code === 'auth/popup-closed-by-user') {
+                    alert('Authentication popup was closed before sign in was completed. Please try again.');
+                }
+            });
+        });
+
+        document.getElementById('logout-btn').addEventListener('click', () => {
+            signOut(auth).then(() => {
+                // user signed out
+                console.log('User signed out.');
+                document.querySelectorAll('.progress-bar').forEach(bar => bar.style.display = 'none');
+            }).catch(error => {
+                // handle errors
+                console.error('Error during sign out: ', error);
+            });
+        });
+
+        onAuthStateChanged(auth, user => {
+            if (user) {
+                // user is signed in
+                loadProgress(user.uid).then(progressData => {
+                    if (progressData) {
+                        console.log(progressData);
+                        updateProgressBars(progressData);
+                    }
+                })
+            } else {
+                console.log('No user is signed in');
+                hideProgressBars();            }
+        });
 
         async function saveProgress(uid, progressData) {
             try {
@@ -68,53 +95,39 @@ getConfig().then(firebaseConfig => {
             }
         }
 
-        document.getElementById('login-btn').addEventListener('click', () => {
-            signInWithPopup(auth, provider).then(result => {
-                const credential = GoogleAuthProvider.credentialFromResult(result);
-                const token = credential.accessToken;
-                const user = result.user;
-                console.log('User signed in: ', user);
-                currentUser = result.user;
-            }).catch(error => {
-                if (error.code === 'auth/popup-closed-by-user') {
-                    console.error('Error during sign in: Popup closed by user');
-                } else {
-                    console.error('Error during sign in: ', error)
+        function updateProgressBars(progressData) {
+            progressData.forEach((value, index) => {
+                const progressBar = document.getElementById(`progress-bar-${index + 1}`);
+                const percentage = document.getElementById(`percentage-${index + 1}`);
+                if (progressBar && percentage) {
+                    progressBar.value = value;
+                    percentage.textContent = `${value}%`;
+                    percentage.style.width = `${value}%`;
                 }
             });
-        });
+        }
 
-        onAuthStateChanged(auth, user => {
-            if (user) {
-                // user is signed in
-                loadProgress(user.uid).then(progressData => {
-                    if (progressData) {
-                        console.log(progressData);
-                        // Update the UI with loaded progress data
-                        progressData.forEach((value, index) => {
-                            const progressBar = document.getElementById(`progress-bar-${index + 1}`);
-                            const percentage = document.getElementById(`percentage-${index + 1}`);
-                            if (progressBar && percentage) {
-                                progressBar.value = value;
-                                percentage.textContent = `${value}%`;
-                                percentage.style.width = `${value}%`;
-                            }
-                        });
-                    }
-                })
-            } else {
-                document.querySelectorAll('.progress-bar').forEach(bar => bar.style.display = 'none');
-            }
-        });
+        function hideProgressBars() {
+            document.querySelectorAll('.progress-bar').forEach(bar => {
+                bar.style.display = 'none';
+            });
+        }
 
-        document.getElementById('logout-btn').addEventListener('click', () => {
-            signOut(auth).then(() => {
-                // user signed out
-                console.log('User signed out.');
-                document.querySelectorAll('.progress-bar').forEach(bar => bar.style.display = 'none');
-            }).catch(error => {
-                // handle errors
-                console.error('Error during sign out: ', error);
+        document.querySelectorAll('.progress-bar').forEach(bar => {
+            bar.addEventListener('input', function() {
+                const percentage = document.getElementById(`percentage-${this.id.split('-')[2]}`);
+                if (percentage) {
+                    percentage.textContent = `${this.value}%`;
+                    percentage.style.width = `${this.value}%`;
+                } else {
+                    console.error('Element not found:', `percentage-${this.id.split('-')[2]}`)
+                }
+
+                const user = auth.currentUser;
+                if (user) {
+                    const progressData = Array.from(document.querySelectorAll('.progress-bar')).map(bar => bar.value);
+                    saveProgress(user.uid, progressData);
+                }
             });
         });
     }
