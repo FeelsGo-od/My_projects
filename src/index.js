06f006cf15833e02fcd26830f0c73063f6629760
FeelsGo-od <andrew.sokolowsky@gmail.com>
2024-05-26 10:48:20 +0200

    attempt to fix coep issue

--- src/index.js
+++ src/index.js
@@ -1,80 +1,136 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from 'firebase/auth';
-import { getFirestore, doc, getDoc } from 'firebase/firestore';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
+import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/src/service-worker.js').then(function(registration) {
-      console.log('Service Worker registered with scope: ', registration.scope);
-    }, function(error) {
-      console.log('Service Worker registration failed:', error);
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/src/service-worker.js').then(function(registration) {
+            console.log('Service Worker registered with scope: ', registration.scope);
+        }, function(error) {
+            console.log('Service Worker registration failed:', error);
+        });
     });
-  });
 }
-
-document.querySelectorAll('.progress-bar').forEach(bar => {
-  bar.addEventListener('input', function() {
-    const percentage = document.getElementById(`percentage-${this.id.split('-')[2]}`);
-    percentage.textContent = `${this.value}%`;
-  });
-});
 
 async function getConfig() {
-  const response = await fetch('/.netlify/functions/config');
-  const config = await response.json();
-  return config.firebaseConfig;
+    const response = await fetch('/.netlify/functions/config');
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const config = await response.json();
+    return config.firebaseConfig;
 }
 
+// let currentUser = null;
+
 getConfig().then(firebaseConfig => {
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-  const provider = new GoogleAuthProvider();
-  const db = getFirestore(app);
+    // Initialize Firebase with the fetched config
+    if (firebaseConfig) {
+        const app = initializeApp(firebaseConfig);
+        const auth = getAuth(app);
+        const provider = new GoogleAuthProvider();
+        const db = getFirestore(app);
 
-  document.getElementById('login-btn').addEventListener('click', () => {
-    signInWithPopup(auth, provider).then(result => {
-      const user = result.user;
-      console.log('User signed in:', user);
-    }).catch(error => {
-      console.error('Error during sign in:', error);
-    });
-  });
+        document.getElementById('login-btn').addEventListener('click', () => {
+            signInWithPopup(auth, provider).then(result => {
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                const user = result.user;
+                console.log('User signed in: ', user);
+            }).catch(error => {
+                console.error('Error during sign in:', error);
+                if (error.code === 'auth/popup-closed-by-user') {
+                    alert('Authentication popup was closed before sign in was completed. Please try again.');
+                }
+            });
+        });
 
-  onAuthStateChanged(auth, user => {
-    if (user) {
-      console.log('User signed in:', user);
-      loadProgress(user.uid);
-    } else {
-      console.log('No user signed in');
-      document.querySelectorAll('.progress-bar').forEach(bar => {
-        bar.style.display = 'none';
-      });
+        document.getElementById('logout-btn').addEventListener('click', () => {
+            signOut(auth).then(() => {
+                // user signed out
+                console.log('User signed out.');
+                document.querySelectorAll('.progress-bar').forEach(bar => bar.style.display = 'none');
+            }).catch(error => {
+                // handle errors
+                console.error('Error during sign out: ', error);
+            });
+        });
+
+        onAuthStateChanged(auth, user => {
+            if (user) {
+                // user is signed in
+                loadProgress(user.uid).then(progressData => {
+                    if (progressData) {
+                        console.log(progressData);
+                        updateProgressBars(progressData);
+                    }
+                })
+            } else {
+                console.log('No user is signed in');
+                hideProgressBars();            }
+        });
+
+        async function saveProgress(uid, progressData) {
+            try {
+                await setDoc(doc(db, 'users', uid), { progressData });
+            } catch (e) {
+                console.error('Error adding document: ', e);
+            }
+        }
+
+        async function loadProgress(uid) {
+            try {
+                const docRef = doc(db, 'users', uid);
+                const docSnap = await getDoc(docRef);
+
+                if (docSnap.exists()) {
+                    return docSnap.data().progressData;
+                } else {
+                    console.log('No progress data found.');
+                    return null;
+                }
+            } catch (e) {
+                console.error('Error loading progress: ', e);
+                return null;
+            }
+        }
+
+        function updateProgressBars(progressData) {
+            progressData.forEach((value, index) => {
+                const progressBar = document.getElementById(`progress-bar-${index + 1}`);
+                const percentage = document.getElementById(`percentage-${index + 1}`);
+                if (progressBar && percentage) {
+                    progressBar.value = value;
+                    percentage.textContent = `${value}%`;
+                    percentage.style.width = `${value}%`;
+                }
+            });
+        }
+
+        function hideProgressBars() {
+            document.querySelectorAll('.progress-bar').forEach(bar => {
+                bar.style.display = 'none';
+            });
+        }
+
+        document.querySelectorAll('.progress-bar').forEach(bar => {
+            bar.addEventListener('input', function() {
+                const percentage = document.getElementById(`percentage-${this.id.split('-')[2]}`);
+                if (percentage) {
+                    percentage.textContent = `${this.value}%`;
+                    percentage.style.width = `${this.value}%`;
+                } else {
+                    console.error('Element not found:', `percentage-${this.id.split('-')[2]}`)
+                }
+
+                const user = auth.currentUser;
+                if (user) {
+                    const progressData = Array.from(document.querySelectorAll('.progress-bar')).map(bar => bar.value);
+                    saveProgress(user.uid, progressData);
+                }
+            });
+        });
     }
-  });
-
-  async function loadProgress(uid) {
-    try {
-      const docRef = doc(db, 'users', uid);
-      const docSnap = await getDoc(docRef);
-      if (docSnap.exists()) {
-        const progressData = docSnap.data().progressData;
-        console.log('Loaded progress data:', progressData);
-      } else {
-        console.log('No such document!');
-      }
-    } catch (error) {
-      console.error('Error loading document:', error);
-    }
-  }
-
-  document.getElementById('logout-btn').addEventListener('click', () => {
-    signOut(auth).then(() => {
-      console.log('User signed out');
-    }).catch(error => {
-      console.error('Error during sign out:', error);
-    });
-  });
+}).catch(error => {
+    console.error('Error fetching Firebase config: ', error);
 });
