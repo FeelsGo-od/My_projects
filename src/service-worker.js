5ae5a74ac20bf9d69aaf168550bbeec33a785cf9
FeelsGo-od <andrew.sokolowsky@gmail.com>
2024-06-01 20:42:20 +0200

    editing service-worker to insure it updates cache

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,20 +1,56 @@
+const CACHE_NAME = 'static-cache-v1';
+const FILES_TO_CACHE = [
+  '../index.html',
+  '../styles.css',
+  '../src/index.js',
+  '../manifest.json'
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('static-cache').then(cache => {
-      return cache.addAll([
-        '../index.html',
-        '../styles.css',
-        '../src/index.js',
-        '../manifest.json'
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
     })
-  );
+  )
+})
+
+self.addEventListener('fetch', event => {
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return caches.open(CACHE_NAME).then(cache => {
+          return fetch(event.request).then(response => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            })
+          })
+        })
+      })
+    )
+  } else {
+    event.respondWith(fetch(event.request));
+  }
+  // event.respondWith(
+  //   caches.match(event.request).then(response => {
+  //     return response || fetch(event.request);
+  //   })
+  // );
 });
